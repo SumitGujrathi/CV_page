8617ae1f3736755f21274266d7ac84bf10cf4dd3
SumitGujrathi <112178227+SumitGujrathi@users.noreply.github.com>
2022-09-11 01:54:53 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+
+
+
+$(".catSlider").on("click", function(){
+    $(".hideData").slideDown();
+});
+
+
+$("img").click(function(){
+    $('.catSlider').css("position", "relative");
+    $('.catSlider').css("z-index", "1");
+   
+
+    $('.catSlider').css("margin", "3% 5% auto 10%");
+
+});
+
+
